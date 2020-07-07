c6838b3e7b5f4f087276802de200a936ccd40008
Collin Kleest <collinkleest@gmail.com>
2020-07-07 10:32:44 -0400

    get rid of landingpads map

--- src/Components/Main/App.js
+++ src/Components/Main/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import NavElement from '../Nav/NavElement/NavElement';
 import NavBar from '../Nav/NavBar/NavBar';
 import Home from './Home';
-import Landing_Pads from '../Landing_Pads/Landing_Pads.js';
 import Rockets from '../Rockets/Rockets';
-import MapContainer from '../Landing_Pads/MapContainer.js';
 import Missions from '../Missons/Missions';
 
 export default function App(){
@@ -43,7 +41,6 @@ export default function App(){
             </NavBar>
             {selection === 'home' && <Home />}
             {selection === 'Rockets' && <Rockets />}
-            {selection === 'Landing Pads' && <MapContainer />}
             {selection === 'Missions' && <Missions />}
         </div>
     );
