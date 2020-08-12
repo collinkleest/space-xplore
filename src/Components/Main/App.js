921fd9f20295d962b4fb6fc59f601f081bb02a54
Collin Kleest <collinkleest@gmail.com>
2020-08-12 19:03:31 -0400

    added react-router

--- src/Components/Main/App.js
+++ src/Components/Main/App.js
@@ -1,59 +1,81 @@
-import React from 'react';
-import NavElement from '../Nav/NavElement/NavElement';
-import NavBar from '../Nav/NavBar/NavBar';
-import Home from './Home';
-import Rockets from '../Rockets/Rockets';
-import Missions from '../Missons/Missions';
-import LandingPads from '../LandingPads/LandingPads';
-import History from '../History/History';
-import MapContainer from '../LaunchPads/MapContainer';
-import Launches from '../Launches/Launches';
+import React from "react";
+import NavElement from "../Nav/NavElement/NavElement";
+import NavBar from "../Nav/NavBar/NavBar";
+import Home from "./Home";
+import Rockets from "../Rockets/Rockets";
+import Missions from "../Missons/Missions";
+import LandingPads from "../LandingPads/LandingPads";
+import History from "../History/History";
+import MapContainer from "../LaunchPads/MapContainer";
+import Launches from "../Launches/Launches";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 
 export default function App() {
+  const [selection, setSelection] = React.useState("home");
 
-    const [selection, setSelection] = React.useState('home');
+  return (
+    <div>
+      <HashRouter>
+        <NavBar onClick={(sel) => setSelection(sel)}>
+          <NavElement
+            linkText="Rockets"
+            relativePath="/rockets"
+            onClick={(sel) => setSelection(sel)}
+          />
 
-    return (
-        <div>
-            <NavBar onClick={(sel) => setSelection(sel)}>
+          <NavElement
+            linkText="Landing Pads"
+            relativePath="/landing-pads"
+            onClick={(sel) => setSelection(sel)}
+          />
 
-                <NavElement
-                    linkText="Rockets"
-                    onClick={(sel) => setSelection(sel)}
-                />
+          <NavElement
+            linkText="Launch Pads"
+            relativePath="/launch-pads"
+            onClick={(sel) => setSelection(sel)}
+          />
 
-                <NavElement
-                    linkText="Landing Pads"
-                    onClick={(sel) => setSelection(sel)}
-                />
+          <NavElement
+            linkText="Missions"
+            relativePath="/missions"
+            onClick={(sel) => setSelection(sel)}
+          />
 
-                <NavElement
-                    linkText="Launch Pads"
-                    onClick={(sel) => setSelection(sel)}
-                />
-
-                <NavElement
-                    linkText="Missions"
-                    onClick={(sel) => setSelection(sel)}
-                />
-
-                <NavElement
-                    linkText="History"
-                    onClick={(sel) => setSelection(sel)}
-                />
-                <NavElement
-                    linkText="Launches"
-                    onClick={(sel) => setSelection(sel)}
-                />
-
-            </NavBar>
-            {selection === 'home' && <Home />}
-            {selection === 'Rockets' && <Rockets />}
-            {selection === 'Missions' && <Missions />}
-            {selection === 'Landing Pads' && <LandingPads />}
-            {selection === 'Launch Pads' && <MapContainer />}
-            {selection === 'History' && <History />}
-            {selection === 'Launches' && <Launches />}
-        </div>
-    );
+          <NavElement
+            linkText="History"
+            relativePath="/history"
+            onClick={(sel) => setSelection(sel)}
+          />
+          <NavElement
+            linkText="Launches"
+            relativePath="/launches"
+            onClick={(sel) => setSelection(sel)}
+          />
+        </NavBar>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/rockets">
+            <Rockets />
+          </Route>
+          <Route path="/missions">
+            <Missions />
+          </Route>
+          <Route path="/landing-pads">
+            <LandingPads />
+          </Route>
+          <Route path="/launch-pads">
+            <MapContainer />
+          </Route>
+          <Route path="/history">
+            <History />
+          </Route>
+          <Route path="/launches">
+            <Launches />
+          </Route>
+        </Switch>
+      </HashRouter>
+    </div>
+  );
 }
